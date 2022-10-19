03b56a0267b6a7cb1a33158906b3c2cdf7516e9d
Pawel <xakrus@bk.ru>
2022-10-19 20:04:59 +0300

    DZ_ALL

--- js/dz.js
+++ js/dz.js
@@ -1,20 +1,20 @@
 //Что выведет в консоль код?
 
- console.log( NaN || 2 || undefined );
+ console.log( NaN || 2 || undefined );  //2
  
-console.log( NaN && 2 && undefined );
+console.log( NaN && 2 && undefined ); //NaN
  
-console.log( 1 && 2 && 3 );
+console.log( 1 && 2 && 3 ); //3
  
-console.log( !1 && 2 || !3 );
+console.log( !1 && 2 || !3 ); //false
  
-console.log( 25 || null && !3 );
+console.log( 25 || null && !3 ); //25
  
-console.log( NaN || null || !3 || undefined || 5);
+console.log( NaN || null || !3 || undefined || 5); //5
  
-console.log( NaN || null && !3 && undefined || 5);
+console.log( NaN || null && !3 && undefined || 5); //5
  
-console.log( 5 === 5 && 3 > 1 || 5);
+console.log( 5 === 5 && 3 > 1 || 5); //true
  
 
 //Выполняется ли условие?
@@ -26,7 +26,7 @@ const nuggets = 2;
  
 if (hamburger === 3 && cola || fries === 3 && nuggets) {
    console.log('Done!')
-}
+}//Выполняется
 
 //Выполняется ли условие?
 
@@ -37,7 +37,7 @@ const nuggets = 2;
  
 if (hamburger || cola || fries === 3 || nuggets) {
    console.log('Done!')
-}
+}//Выполняется
 
 //Переменная hamburger задана через let, так как const подразумевает какое-либо начальное значение. С const будет ошибка, можете проверить.
 //А без значения там undefined, напоминаю 🙂
@@ -51,4 +51,4 @@ const nuggets = 2;
  
 if (hamburger && cola || fries === 3 && nuggets) {
    console.log('Done!')
-}
+}//Не выполняется
